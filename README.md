50619ac2515c0e9ae35b83cca2ac3e44a8deb043
Ondřej Kubíček <ondrakub@gmail.com>
2013-12-28 13:26:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+Password Preview
+-----------------------------
+
+Google Chrome extension to view passwords on any site
+
+Features
+========
+
+* Scrobbles now playing songs at 70% played time
+* Song information in popup window (artist, title, time)
+* Cloud Player state in icon
+
+Installation
+============
+
+Install stable release from [Google Web Store](https://chrome.google.com/webstore/detail/preview-password/pafeihdhoahmjhcagiegdnohbkmpmllb)
+
+Version History
+===============
+
+**Version 1.3.2**
+Preview realese for testers
+
+
+Author
+======
+
+[Ondřej Kubíček](https://github.com/ondrakub), ondrakub@gmail.com
---
+++ js/password.js
@@ -0,0 +1,23 @@
+var id = chrome.i18n.getMessage('@@extension_id');
+$inputs = $('input[type=password]');
+
+$inputs.each(function(){
+	var $input = $(this),
+		$span = $('<span />').css({'position':'absolute', 'right':'5%', 'top': '10%', 'width': 16, 'height': 16, 'background': 'url(chrome-extension://'+id+'/images/icon.png) right top no-repeat'}),
+		$wrap = $('<span />').css({'position':'relative'});
+		timeoutId = 0;
+
+	$input.wrap($wrap);
+	$input.after($span);
+
+	$span.mousedown(function() {
+	    timeoutId = setTimeout(function(){
+	    	var style = css($input);
+	    	$input.attr('type', 'text');
+	    	$input.css(style);
+	    }, 100);
+	}).bind('mouseup mouseleave', function() {
+		$input.attr('type', 'password');
+	    clearTimeout(timeoutId);
+	});
+});
